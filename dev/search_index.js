c597260ccac3c15e9fa4ac31499528639acfda2e
Documenter.jl <documenter@juliadocs.github.io>
2022-04-09 03:25:24 +0000

    build based on 4c6e605

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = PhyloCoalSimulations","category":"page"},{"location":"#PhyloCoalSimulations","page":"Home","title":"PhyloCoalSimulations","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for PhyloCoalSimulations.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [PhyloCoalSimulations]","category":"page"}]
+}
